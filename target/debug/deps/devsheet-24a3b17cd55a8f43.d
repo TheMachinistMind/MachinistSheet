ac4b9b6e2a10a9aae6fd5d7864d52eb19ce285fc
TheMachinistMind <povtherlog@gmail.com>
2026-07-01 17:21:57 +0200

    add gitignore, remove target folder

--- target/debug/deps/devsheet-24a3b17cd55a8f43.d
+++
@@ -1,5 +0,0 @@
-/Users/povther/Desktop/devsheet/target/debug/deps/devsheet-24a3b17cd55a8f43.d: src/main.rs
-
-/Users/povther/Desktop/devsheet/target/debug/deps/devsheet-24a3b17cd55a8f43: src/main.rs
-
-src/main.rs:
--- target/debug/devsheet.d
+++
@@ -1,1 +0,0 @@
-/Users/povther/Desktop/devsheet/target/debug/devsheet: /Users/povther/Desktop/devsheet/src/main.rs
